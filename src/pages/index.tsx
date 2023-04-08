dd6be70c911c3f7474b8be2a3a127037ee44748c
JimS <jimsun6428@gmail.com>
2023-04-08 14:25:18 +0800

    loading bug fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { RouterOutputs, api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage } from "~/components/loadingPage";
 
 dayjs.extend(relativeTime);
 
