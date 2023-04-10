c1eb13570d0df2e780bb1a294bbca4742a8c4aba
JimS <jimsun6428@gmail.com>
2023-04-10 17:03:21 +0800

    profile page with ssg helper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { LoadingPage, LoadingSpinner } from "~/components/loadingPage";
+import { PageLayout } from "~/components/layout";
 
 dayjs.extend(relativeTime);
 
@@ -130,20 +131,18 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <main className="flex h-screen justify-center ">
-        <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
-          <div className="flex border-b border-slate-400 p-4">
-            {!isSignedIn && (
-              <div className="flex justify-center">
-                <SignInButton />
-              </div>
-            )}
-            {isSignedIn && <CreatePostWizard />}
-          </div>
-          <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-          <Feed />
+      <PageLayout>
+        <div className="flex border-b border-slate-400 p-4">
+          {!isSignedIn && (
+            <div className="flex justify-center">
+              <SignInButton />
+            </div>
+          )}
+          {isSignedIn && <CreatePostWizard />}
         </div>
-      </main>
+        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <Feed />
+      </PageLayout>
     </>
   );
 };
